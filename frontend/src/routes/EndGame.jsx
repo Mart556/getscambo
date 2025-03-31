fa5aac726322819e880e1b916d1470eb8c12ff4b
Mart556 <marthaamer123@gmail.com>
2025-03-31 20:33:32 +0300

    Refactor EndGame component to streamline meme fetching and result saving logic

--- frontend/src/routes/EndGame.jsx
+++ frontend/src/routes/EndGame.jsx
@@ -4,111 +4,116 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRepeat, faHome } from "@fortawesome/free-solid-svg-icons";
 
 const EndReasons = {
-    ["time"]: "Aeg sai otsa!",
-    ["answer"]: "Vastasid valesti!",
+	["time"]: "Aeg sai otsa!",
+	["answer"]: "Vastasid valesti!",
 };
 
 const EndGame = ({ isNewHighScore, endReason }) => {
-    const [endReasonText, setEndReasonText] = useState("Mäng läbi!");
+	const [endReasonText, setEndReasonText] = useState("Mäng läbi!");
 
-    useEffect(() => {
-        setEndReasonText(EndReasons[endReason] || "Mäng läbi!");
-    }, [endReason]);
+	useEffect(() => {
+		setEndReasonText(EndReasons[endReason] || "Mäng läbi!");
+	}, [endReason]);
 
-    useEffect(() => {
-        fetchRandomMeme();
+	const [meme, setMeme] = useState("");
 
-        const saveResult = () => {
-            fetch("/api/submit-score", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    username: localStorage.getItem("username"),
-                    score: localStorage.getItem("highestPoints"),
-                }),
-            })
-                .then((response) => response.json())
-                .then((data) => {
-                    console.log("Result saved:", data);
+	useEffect(() => {
+		const fetchRandomMeme = async () => {
+			try {
+				const response = await fetch(
+					"https://api.imgflip.com/get_memes"
+				);
+				const { success, data } = await response.json();
+				if (success) {
+					const memes = data.memes;
+					const randomIndex = Math.floor(
+						Math.random() * memes.length
+					);
+					const selectedMeme = memes[randomIndex];
 
-                    localStorage.setItem(
-                        "lastHighScore",
-                        localStorage.getItem("highestPoints") || 0
-                    );
+					setMeme(selectedMeme);
 
-                    alert("Uus rekord on salvestatud!");
-                })
-                .catch((error) => {
-                    console.error("Error saving result:", error);
-                });
-        };
+					const img = new Image();
+					img.src = selectedMeme.url;
+				}
+			} catch (error) {
+				console.error("Error fetching meme:", error);
+			}
+		};
 
-        if (isNewHighScore) saveResult();
-    }, [isNewHighScore]);
+		fetchRandomMeme();
 
-    const [meme, setMeme] = useState("");
+		const saveResult = () => {
+			fetch("/api/submit-score", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					username: localStorage.getItem("username"),
+					score: localStorage.getItem("highestPoints"),
+				}),
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					console.log("Result saved:", data);
 
-    const fetchRandomMeme = async () => {
-        try {
-            const response = await fetch("https://api.imgflip.com/get_memes");
-            const { success, data } = await response.json();
-            if (success) {
-                const memes = data.memes;
-                const randomIndex = Math.floor(Math.random() * memes.length);
-                const selectedMeme = memes[randomIndex];
+					localStorage.setItem(
+						"lastHighScore",
+						localStorage.getItem("highestPoints") || 0
+					);
 
-                const img = new Image();
-                img.src = selectedMeme.url;
-                img.onload = () => setMeme(selectedMeme);
-            }
-        } catch (error) {
-            console.error("Error fetching meme:", error);
-        }
-    };
+					alert("Uus rekord on salvestatud!");
+				})
+				.catch((error) => {
+					console.error("Error saving result:", error);
+				});
+		};
 
-    return (
-        <div className="flex flex-col items-center justify-between h-full bg-neutral-800/75  backdrop-filter backdrop-blur-lg  rounded-lg shadow-lg p-4 my-4">
-            <div className="flex flex-col justify-center items-center w-full">
-                <h1 className="text-[2.5rem] md:text-6xl font-bold bg-gradient-to-r from-red-400 to-violet-300 text-transparent bg-clip-text">
-                    {endReasonText}
-                </h1>
-            </div>
+		if (isNewHighScore) saveResult();
+	}, [isNewHighScore]);
 
-            <div className="flex flex-col justify-center items-center w-full">
-                <img
-                    src={meme.url}
-                    alt={meme.name}
-                    className="rounded-lg shadow-lg w-full h-auto max-w-xs max-h-[360px] my-4"
-                />
-            </div>
+	return (
+		<div className='flex flex-col items-center justify-between h-full bg-neutral-800/75  backdrop-filter backdrop-blur-lg  rounded-lg shadow-lg p-4 my-4'>
+			<div className='flex flex-col justify-center items-center w-full'>
+				<h1 className='text-[2.5rem] md:text-6xl font-bold bg-gradient-to-r from-red-400 to-violet-300 text-transparent bg-clip-text'>
+					{endReasonText}
+				</h1>
+			</div>
 
-            <div className="flex flex-col sm:flex-row justify-center items-center w-full sm:w-1/2">
-                <button
-                    type="button"
-                    className="bg-blue-500 text-white font-bold py-5  w-75 rounded m-3 text-2xl cursor-pointer"
-                    onClick={() => window.location.reload()}
-                >
-                    <FontAwesomeIcon
-                        icon={faRepeat}
-                        size="xl"
-                        className="me-2"
-                    />{" "}
-                    Uuesti
-                </button>
+			<div className='flex flex-col justify-center items-center w-full'>
+				<img
+					src={meme.url}
+					alt={meme.name}
+					className='rounded-lg shadow-lg w-full h-auto max-w-xs max-h-[360px] my-4'
+				/>
+			</div>
 
-                <button
-                    type="button"
-                    className="bg-red-500 text-white font-bold py-5 w-75 rounded m-3 text-2xl cursor-pointer"
-                    onClick={() => window.location.replace("/")}
-                >
-                    <FontAwesomeIcon icon={faHome} size="xl" className="me-2" />
-                    Tagasi
-                </button>
-            </div>
-        </div>
-    );
+			<div className='flex flex-col sm:flex-row justify-center items-center w-full sm:w-1/2'>
+				<button
+					type='button'
+					className='bg-blue-500 text-white font-bold py-5  w-75 rounded m-3 text-2xl cursor-pointer'
+					onClick={() => window.location.reload()}
+				>
+					<FontAwesomeIcon
+						icon={faRepeat}
+						size='xl'
+						className='me-2'
+					/>{" "}
+					Uuesti
+				</button>
+
+				<button
+					type='button'
+					className='bg-red-500 text-white font-bold py-5 w-75 rounded m-3 text-2xl cursor-pointer'
+					onClick={() => window.location.replace("/")}
+				>
+					<FontAwesomeIcon icon={faHome} size='xl' className='me-2' />
+					Tagasi
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default EndGame;
